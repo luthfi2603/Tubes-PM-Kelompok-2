cb9f2396ef43dafb97f1db102ae6a90e55a5b482
luthfi2603 <luthfim904@gmail.com>
2024-11-26 14:02:11 +0700

    buat halaman login

---
+++ app/src/main/java/com/pmkomc22kelompok2/bookjas/ui/login/ui/login/LoggedInUserView.kt
@@ -0,0 +1,9 @@
+package com.pmkomc22kelompok2.bookjas.ui.login.ui.login
+
+/**
+ * User details post authentication that is exposed to the UI
+ */
+data class LoggedInUserView(
+    val displayName: String
+    //... other data fields that may be accessible to the UI
+)
---
+++ app/src/main/java/com/pmkomc22kelompok2/bookjas/ui/login/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.pmkomc22kelompok2.bookjas.ui.login.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
