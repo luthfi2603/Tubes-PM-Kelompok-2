51d0c59056908e5fbccfc077451658579e99ef1d
luthfi2603 <luthfim904@gmail.com>
2024-12-01 12:08:19 +0700

    buat halaman riwayat peminjaman di level admin

---
+++ app/src/main/java/com/pmkomc22kelompok2/bookjas/ui/admin/riwayatpeminjaman/ListRiwayatPeminjamAdapter.kt
@@ -0,0 +1,27 @@
+package com.pmkomc22kelompok2.bookjas.ui.admin.riwayatpeminjaman
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.pmkomc22kelompok2.bookjas.databinding.ItemRiwayatPeminjamBinding
+import java.util.Locale
+
+class ListRiwayatPeminjamAdapter(private val listPeminjam: ArrayList<RiwayatPeminjam>) : RecyclerView.Adapter<ListRiwayatPeminjamAdapter.ListViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        return ListViewHolder(ItemRiwayatPeminjamBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun getItemCount(): Int = listPeminjam.size
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val (foto, judulBuku, author, peminjam, tanggalPeminjaman, tanggalPengembalian) = listPeminjam[position]
+        holder.binding.ivFotoBuku.setImageResource(foto)
+        holder.binding.tvJudulBuku.text = judulBuku
+        holder.binding.tvAuthor.text = author
+        holder.binding.tvPeminjam.text = peminjam
+        holder.binding.tvTanggalPeminjaman.text = tanggalPeminjaman
+        holder.binding.tvTanggalPengembalian.text = tanggalPengembalian
+    }
+
+    inner class ListViewHolder(val binding: ItemRiwayatPeminjamBinding) : RecyclerView.ViewHolder(binding.root)
+}
